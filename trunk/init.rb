fce8596faf42c026232b3e5f57490397e9193560
keithm <keithm@694b6922-ce17-0410-93af-c5f0f3c483be>
2006-07-24 06:27:20 +0000

    git-svn-id: http://www.infused.org/svn/plugins/test_injector@18 694b6922-ce17-0410-93af-c5f0f3c483be

--- trunk/init.rb
+++ trunk/init.rb
@@ -1,1 +1,8 @@
-require 'test_injector'
+if RAILS_ENV == 'test'  
+  require 'test_injector'
+  require 'test/unit'
+
+  Test::Unit::TestCase.class_eval do
+    include TestInjector
+  end
+end
--- trunk/lib/test_injector.rb
+++ trunk/lib/test_injector.rb
@@ -20,115 +20,121 @@
 
 module TestInjector
   
-  def base_class
-    @base_class ||= Module.const_get self.to_s.gsub(/Test$/, '')
+  def self.included(base)
+    base.extend ClassMethods
   end
   
-  def inject_activerecord_tests
-    if base_class.ancestors.include?(ActiveRecord::Base)
-      inject_association_tests if base_class.respond_to?(:reflections)
-      define_acts_as_versioned_test(base_class) if base_class.respond_to?("acts_as_versioned") and base_class.include?(ActiveRecord::Acts::Versioned::ActMethods)      
-      define_optimistic_locking_test(base_class) if base_class.column_names.include?("lock_version")
-    end
+  def klass
+    @klass ||= Module.const_get(self.class.to_s.gsub(/Test$/, ''))
   end
   
-  def inject_association_tests
-    ignore_associations = [:versions, :parent, :children]
-    collectible_associations = [:has_many, :has_and_belongs_to_many]
+  # Get all descendants of an acts_as_tree node
+  def all_descendants(node, id_only = true)
+    nodes = node.children.each {|subnode| all_descendants(subnode)}.flatten
+    return id_only ? nodes.collect {|node| node.id} : nodes
+  end
+  
+  module ClassMethods
     
-    base_class.reflect_on_all_associations.each do |association|
-      
-      # redefine base_class as an instance method
-      define_method "base_class" do
-        @base_class ||= Module.const_get self.class.to_s.gsub(/Test$/, '')
-      end
-      
-      unless ignore_associations.include?(association.name)
-        define_fixture_test(association)
-        define_association_test(association)
-      end
-
+    def klass
+      @klass ||= Module.const_get(self.to_s.gsub(/Test$/, ''))
     end
-
-  end
+    
+    def inject_activerecord_tests
+      if klass.ancestors.include?(ActiveRecord::Base)
+        inject_association_tests
+        define_acts_as_versioned_test(klass) if klass.respond_to?(:acts_as_versioned) and klass.respond_to?(:versioned_table_name)
+        define_optimistic_locking_test(klass) if klass.column_names.include?("lock_version") && ActiveRecord::Base.lock_optimistically == true
+      end
+    end
   
-  def define_association_test(association)
-    collectible_associations = [:has_many, :has_and_belongs_to_many]
-    association_name = association.options[:through] ? "#{association.name}_through_#{association.options[:through]}" : association.name
-    remove_method "test_#{association.macro}_#{association_name}" rescue
-    define_method "test_#{association.macro}_#{association_name}" do
-      associated_model = eval(association.class_name)
-      model_instance = base_class.find(:first)
-      
-      # tests for associations that return a collection of objects
-      if collectible_associations.include?(association.macro)
-        assert_kind_of Array, model_instance.send(association.name), "#{base_class}##{association.name} expected an array of #{association.class_name}'s"
-        assert_kind_of associated_model, model_instance.send(association.name).first, 
-          "#{base_class}##{association.name} was expected to be a #{association.class_name}. If the result is a NilClass, verify your fixtures."
-        if [:destroy, :delete_all].include?(association.options[:dependent])
-          association_count = model_instance.send(association.name).size
-          associated_record_count = associated_model.count
-          assert model_instance.destroy
-          message = "Unexpected result when calling #{association.options[:dependent]} on dependent association :#{association.name}"
-          if base_class.include?(ActiveRecord::Acts::Tree) && model_instance.respond_to?(:children)
-            assert associated_record_count - association_count >= associated_model.count, message
-          else
-            assert_equal associated_record_count - association_count, associated_model.count, message
+    def inject_association_tests
+      ignore_associations = [:versions, :parent, :children]
+      collectible_associations = [:has_many, :has_and_belongs_to_many]
+      klass.reflect_on_all_associations.each do |association|
+        unless ignore_associations.include?(association.name)
+          define_fixture_test(association)
+          define_association_test(association)
+        end
+      end
+    end
+  
+    def define_association_test(association)
+      collectible_associations = [:has_many, :has_and_belongs_to_many]
+      association_name = association.options[:through] ? "#{association.name}_through_#{association.options[:through]}" : association.name
+      remove_method "test_#{association.macro}_#{association_name}" rescue
+      define_method "test_#{association.macro}_#{association_name}" do
+        record = klass.find(:first)
+        # tests for associations that return a collection of objects
+        if collectible_associations.include?(association.macro)
+          assert_kind_of Array, record.send(association.name), "#{klass}##{association.name} expected an array of #{association.class_name}'s"
+          assert_kind_of association.klass, record.send(association.name).first, 
+            "#{klass}##{association.name}.first should have returned a #{association.class_name}. If the result is a NilClass, verify your fixtures."
+          associated_record_ids = record.send(association.name).map {|r| r.id}
+          assert record.destroy
+          msg = "Unexpected result when calling #{association.options[:dependent]} on dependent association :#{association.name}"
+          if [:destroy, :delete_all].include?(association.options[:dependent])
+            associated_record_ids.each do |record_id|
+              assert_raises(ActiveRecord::RecordNotFound, msg) { association.klass.find(record_id) }
+            end
+          elsif association.options[:dependent] == :nullify
+            assert record.destroy
+            associated_record_ids.each do |record_id| 
+              assert [0,'0',nil].include?(association.klass.find(record_id).send(association.primary_key_name)), msg
+            end
           end
-        elsif association.options[:dependent] == :nullify
-          associated_record_ids = model_instance.send(association.name).map {|r| r.id}
-          assert model_instance.destroy
-          associated_record_ids.each {|the_id| assert [0,'0',nil].include?(associated_model.find(the_id).send(association.primary_key_name))}
+      
+        # tests for associations the return a single object
+        else
+          assert_kind_of association.klass, record.send(association.name), 
+            "#{klass}##{association.name} was expected to be a #{association.class_name}. If the result is a NilClass, verify your fixtures."
+          msg = "Unexpected result when calling #{association.options[:dependent]} on dependent association :#{association.name}"
+          if [:destroy, :delete].include?(association.options[:dependent])
+            record_id = record.send(association.name).id
+            assert record.destroy
+            assert_raises(ActiveRecord::RecordNotFound, msg) { association.klass.find(record_id) }
+          end
         end
       
-      # tests for associations the return a single object
-      else
-        assert_kind_of associated_model, model_instance.send(association.name), 
-          "#{base_class}##{association.name} was expected to be a #{association.class_name}. If the result is a NilClass, verify your fixtures."
-        if [:destroy, :delete].include?(association.options[:dependent])
-          dependent_id = model_instance.send(association.name).id
-          assert model_instance.destroy
-          assert_raises(ActiveRecord::RecordNotFound) {associated_model.find(dependent_id)}
+      end
+    end
+    # This test will run for any ActiveRecord model.  For each association defined in the model being tested (belongs_to, has_many, etc),
+    # the test checks to see if a fixture corresponding to the associated model is included in the fixture list.
+    def define_fixture_test(association)
+      define_method "test_fixture_defined_for_#{association.klass.table_name}" do
+        [association.klass.table_name, association.options[:join_table]].compact.each do |table_name|
+          assert fixture_table_names.include?(table_name), "No fixture is defined for :#{table_name}"
         end
       end
-      
     end
-  end
-  # This test will run for any ActiveRecord model.  For each association defined in the model being tested (:belongs_to, :has_many, etc),
-  # the test checks to see if a fixture corresponding to the associated model is included in the fixture list.
-  def define_fixture_test(association)
-    fixture_name = eval(association.class_name).table_name
-    define_method "test_fixture_defined_for_#{fixture_name}" do
-      assert respond_to?(association.options[:join_table]), "No fixture defined for :#{association.options[:join_table]}" if association.options[:join_table]
-      assert respond_to?(fixture_name), "No fixture defined for :#{fixture_name}"
-    end
-  end
   
-  # This test will run for any ActiveRecord model that uses the acts_as_versioned plugin. Versioning may fail if the versioned table is 
-  # not set up correctly.  Including this test insures that versioning is set up correctly and is working as expected.
-  def define_acts_as_versioned_test(base_class)
-    define_method "test_acts_as_versioned" do
-      model = base_class.find(:first)
-      assert_equal 0, model.version
-      assert model.save
-      assert model.reload
-      assert_equal 1, model.version
-      assert_equal model.version, model.versions.size
-    end
-  end
-  
-  # This test will run for any ActiveRecord model that has a lock_version column. Optimistic locking may fail if the lock_version 
-  # column is set to "not null" or does not have a default value.  Including this test insures that the lock_version column is set
-  # up correctly.
-  def define_optimistic_locking_test(base_class)
-    define_method "test_optimistic_locking" do
-      model_instance_1 = base_class.find(:first)
-      model_instance_2 = base_class.find(:first)
-      assert model_instance_1.save
-      unless assert_raises(ActiveRecord::StaleObjectError) { model_instance_2.save }
-        flunk "Optimistic locking is not functioning on the #{base_class.table_name} table. Check configuration of the lock_version column."
+    # This test will run for any ActiveRecord model that uses the acts_as_versioned plugin. Versioning may fail if the versioned table is 
+    # not set up correctly.  Including this test insures that versioning is set up correctly and is working as expected.
+    def define_acts_as_versioned_test(base)
+      define_method "test_acts_as_versioned" do
+        msg = "ActsAsVersioned is not working correctly. Check the configuration of the #{base.table_name} and #{base.versioned_table_name} tables."
+        model = base.find(:first)
+        original_version = model.version
+        assert model.save
+        assert model.reload
+        assert_equal original_version + 1, model.version, msg
+        assert_equal model.version, model.versions.size, msg
       end
     end
+  
+    # This test will run for any ActiveRecord model that has a lock_version column. Optimistic locking may fail if the lock_version 
+    # column is set to "not null" or does not have a default value.  Including this test insures that the lock_version column is set
+    # up correctly.
+    def define_optimistic_locking_test(base)
+      define_method "test_optimistic_locking" do
+        msg = "Optimistic locking is not functioning on the #{base.table_name} table. Check configuration of the lock_version column."
+        model1 = base.find(:first)
+        model2 = base.find(:first)
+        assert model1.save
+        assert_raises(ActiveRecord::StaleObjectError, msg) { model2.save }
+      end
+    end
+    
   end
   
 end
