89d4bb82b1e63401a0ad48e8de854a8250a107dc
keithm <keithm@694b6922-ce17-0410-93af-c5f0f3c483be>
2006-09-10 09:42:30 +0000

    Don't load tidy_functionals be default
    git-svn-id: http://www.infused.org/svn/plugins/test_injector@46 694b6922-ce17-0410-93af-c5f0f3c483be

--- init.rb
+++ init.rb
@@ -1,7 +1,9 @@
 if RAILS_ENV == 'test'  
   require 'test_injector'
   require 'test/unit'
-  require 'tidy_functionals'
+  
+  # tidy_functionals will break the mocha plugin - use at your own risk
+  # require 'tidy_functionals'
 
   Test::Unit::TestCase.class_eval do
     include TestInjector
